994866679bea98e0690b2a15984742d16e4d103b
Reshwanth Naraparaju <reshwanth.naraparaju@volantetech.com>
2026-01-24 10:58:30 +0530

    ec2-security group

---
+++ ec2.tf
@@ -0,0 +1,28 @@
+//
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+   ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
+resource "aws_instance" "terraform-ec2" {
+  ami           = "ami-0532be01f26a3de55"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "reshwanth"
+  }
+}
