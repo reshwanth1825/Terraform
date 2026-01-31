a126060b490748a14c220c018bd56b042e5c3711
Reshwanth Naraparaju <reshwanth.naraparaju@volantetech.com>
2026-01-31 10:37:21 +0530

    ec2-modified

--- ec2.tf
+++ ec2.tf
@@ -3,14 +3,14 @@ resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = var.ingress_from_port
+    to_port     = var.ingress_to_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.ingress_cidr
   }
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = var.egress_from_port
+    to_port     = var.egress_in_port
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
