bdee4054d238c2c51370d7f61c83e63265306449
Reshwanth Naraparaju <reshwanth.naraparaju@volantetech.com>
2026-01-31 10:39:43 +0530

    variable modified

--- variable.tf
+++ variable.tf
@@ -6,3 +6,28 @@ variable "instance_size" {
   type    = string
   default = "t2.micro"
 }
+
+variable "ingress_from_port" {
+  type    = number
+  default = 22
+}
+
+variable "ingress_to_port" {
+  type    = number
+  default = 22
+}
+
+variable "egress_from_port" {
+  type    = number
+  default = 0
+}
+
+variable "egress_in_port" {
+  type    = number
+  default = 0
+}
+
+variable "ingress_cidr" {
+  default = ["0.0.0.0/0"]
+}
+
